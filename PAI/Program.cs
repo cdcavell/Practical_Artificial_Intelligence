c3664d984f21a8b69383cb0cc805233bd9bf5b74
P00157203 - Chris Cavell <chris.cavell@la.gov>
2019-03-12 13:36:58 -0500

    Added Table 1-1: Truth Table for Negation Logical Connective [NOT]

--- PAI/Program.cs
+++ PAI/Program.cs
@@ -7,6 +7,8 @@ namespace PAI
         static void Main(string[] args)
         {
             // Chapter 1 - Logical Connectives
+            Table_1_1.Display();
+            Console.WriteLine(string.Empty);
             Table_1_2.Display();
             Console.WriteLine(string.Empty);
             Table_1_3.Display();
---
+++ PAI/Table_1-1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PAI
+{
+    public static class Table_1_1
+    {
+        public static void Display()
+        {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(" Table 1-1: Truth Table for Negation Logical Connective [NOT]");
+            Console.WriteLine(string.Empty);
+
+            Row.Write("p", "NOT p", "");
+
+            Evaluate(new Variable(true));
+            Evaluate(new Variable(false));
+        }
+
+        private static void Evaluate(Variable p)
+        {
+            Row.Write(p.Value.ToString(), new Not(p).Evaluate().ToString(), string.Empty);
+        }
+    }
+}
