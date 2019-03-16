b0b905aad30b9f26627669d18c1aa169ff20cad3
P00157203 - Chris Cavell <chris.cavell@la.gov>
2019-03-16 02:41:03 -0500

    Reorganize files.

---
+++ PAI/Chapter 1/My Classes/Chapter1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PAI
+{
+    public static class Chapter1
+    {
+        public static void Display()
+        {
+            Table_1_1.Display();
+            Console.WriteLine(string.Empty);
+            Table_1_2.Display();
+            Console.WriteLine(string.Empty);
+            Table_1_3.Display();
+            Console.WriteLine(string.Empty);
+            Table_1_4.Display();
+            Console.WriteLine(string.Empty);
+            Table_1_5.Display();
+            Console.WriteLine(string.Empty);
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Press <Enter> key to exit ...");
+            Console.ReadLine();
+        }
+    }
+}
